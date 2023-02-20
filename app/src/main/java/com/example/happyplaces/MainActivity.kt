519ccccfda85258bc387dc9891e51e124da7ecab
Urahs <sharumaku@gmail.com>
2023-02-20 11:57:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/happyplaces/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.happyplaces
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.happyplaces.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding.fabHappyPlace.setOnClickListener{
+            startActivity(Intent(this,AddHappyPlaceActivity::class.java))
+        }
+    }
+}
